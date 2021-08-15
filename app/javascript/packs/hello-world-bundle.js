e20b39154f1dd9d2baa954be84eef3ed8d648eac
Lyzio Etancelin <edouard.etancelin@gmail.com>
2021-08-15 17:51:27 +0200

    Coucou

--- app/javascript/packs/hello-world-bundle.js
+++ app/javascript/packs/hello-world-bundle.js
@@ -1,8 +1,9 @@
 import ReactOnRails from 'react-on-rails';
 
-import GetOut from '../bundles/HelloWorld/components/GetOut.tsx';
+import HelloWorld from '../bundles/HelloWorld/components/HelloWorld';
+
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
-  GetOut,
+  HelloWorld,
 });
